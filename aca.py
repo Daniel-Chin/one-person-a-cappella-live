d6af32e8e72f2f88e9f81002baa7daa1a70730f7
Daniel Chin <daniel_chin@yahoo.com>
2021-06-01 15:00:20 +0800

    I am speed

--- aca.py
+++ aca.py
@@ -2,6 +2,7 @@ print('importing...')
 import pyaudio
 from time import time, sleep
 import numpy as np
+from numpy.fft import rfft
 import scipy
 from scipy.interpolate import interp1d
 from threading import Lock
@@ -32,7 +33,7 @@ WRITE_FILE = f'demo_{time()}.wav'
 MASTER_VOLUME = .2
 SR = 22050
 NYQUIST = SR // 2
-DTYPE_BUF = (np.float32, pyaudio.paFloat32)
+DTYPE_BUF = np.float32
 DTYPE_IO = (np.int32, pyaudio.paInt32)
 TWO_PI = np.pi * 2
 HANN = scipy.signal.get_window('hann', PAGE_LEN, True)
@@ -45,14 +46,17 @@ INT32RANGE = 2**31
 INV_INT32RANGE = 1 / 2**31
 NOTE_ON = 'note_on'
 NOTE_OFF = 'note_off'
+SR_OVER_PAGE_LEN = SR / PAGE_LEN
 
 streamOutContainer = []
 terminate_flag = 0
 terminateLock = Lock()
 profiler = StreamProfiler(PAGE_LEN / SR, DO_PROFILE)
 notes = []
+notes_changed = True
 notesLock = Lock()
 hySynth = None
+harmonics = []
 
 if DO_PROFILE:
     _print = print
@@ -68,7 +72,7 @@ def main():
     global terminate_flag, f, hySynth
     terminateLock.acquire()
     hySynth = HybridSynth(
-        HYBRID_QUALITY, SR, PAGE_LEN, DTYPE_BUF[0], 
+        HYBRID_QUALITY, SR, PAGE_LEN, DTYPE_BUF, 
     )
     pa = pyaudio.PyAudio()
     # in_i, out_i = selectAudioDevice(pa)
@@ -122,10 +126,21 @@ def main():
 def getEnvelope(signal, len_signal):
     f0 = yin(signal, SR, len_signal, fmax=FMAX)
     harmonics_f = np.arange(0, NYQUIST, f0)
-    harmonics_a = [sft(signal * HANN, f_bin) for f_bin in harmonics_f / SR * len_signal]
+    harmonics_a = np.zeros((harmonics_f.size, ))
+    spectrum_2 = np.square(np.abs(rfft(signal * HANN)))
+    SR_OVER_PAGE_LEN_OVER_f0 = SR_OVER_PAGE_LEN / f0
+    for i, energy_2 in enumerate(spectrum_2):
+        n = round(i * SR_OVER_PAGE_LEN_OVER_f0)
+        if n >= harmonics_f.size:
+            break
+        harmonics_a[n] += energy_2
+    harmonics_a = np.sqrt(harmonics_a) / PAGE_LEN
     harmonics_a[0] = harmonics_a[1]
     f = interp1d(harmonics_f, harmonics_a)
+    # max_f = harmonics_f[-1]
     def envelope(x):
+        # if x >= max_f:
+        #     return 0
         try:
             return f(x)
         except ValueError:
@@ -133,6 +148,7 @@ def getEnvelope(signal, len_signal):
     return envelope
 
 def onAudioIn(in_data, sample_count, *_):
+    global notes_changed
     try:
         if terminate_flag == 1:
             terminateLock.release()
@@ -148,34 +164,42 @@ def onAudioIn(in_data, sample_count, *_):
         page = np.frombuffer(
             in_data, dtype = DTYPE_IO[0]
         )
-        page = np.multiply(page, INV_INT32RANGE, dtype=DTYPE_BUF[0])
+        page = np.multiply(page, INV_INT32RANGE, dtype=DTYPE_BUF)
 
         profiler.gonna('getE')
         envelope = getEnvelope(page, PAGE_LEN)
 
-        profiler.gonna('crit_sec')
+        profiler.gonna('note_in')
         if DEBUG_NO_MIDI:
-            freqs = [300]
-        else:
-            with notesLock:
-                freqs = [pitch2freq(n) for n in notes]
-
-        profiler.gonna('interp')
-        harmonics = []
-        if freqs:
+            if not notes:
+                notes_changed = True
+                notes.append(53)
+        notesLock.acquire()
+        if notes_changed:
+            freqs = [pitch2freq(n) for n in notes]
+            notes_changed = False
+            notesLock.release()
+            harmonics.clear()
             for f0 in freqs:
                 for fn in LADDER * f0:
                     if fn >= NYQUIST:
                         break
                     harmonics.append(
-                        Harmonic(fn, envelope(fn))
+                        Harmonic(fn, 0)
                     )
+            harmonics.sort(key=Harmonic.getFreq)
+        else:
+            notesLock.release()
 
+        profiler.gonna('interp')
+        for harmonic in harmonics:
+            harmonic.mag = envelope(harmonic.freq)
+        
         profiler.gonna('eat')
-        hySynth.eat(harmonics)
+        hySynth.eat(harmonics, skipSort=True)
 
         profiler.gonna('mix')
-        if freqs:
+        if notes:
             mixed = hySynth.mix()
         else:
             mixed = page
@@ -199,11 +223,13 @@ def pitch2freq(pitch):
     return np.exp((pitch + 36.37631656229591) * 0.0577622650466621)
 
 def onMidiIn(msg):
+    global notes_changed
     with notesLock:
         if msg.type == NOTE_ON:
             assert msg.note not in notes
             notes.append(msg.note)
         elif msg.type == NOTE_OFF:
             notes.remove(msg.note)
+        notes_changed = True
 
 main()
