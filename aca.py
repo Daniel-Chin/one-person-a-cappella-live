c38d57a7d0d00d5341e2bbfc8b835ca7ca3b3795
Daniel Chin <daniel_chin@yahoo.com>
2021-07-22 10:59:31 +0800

    dependency error output

--- aca.py
+++ aca.py
@@ -18,9 +18,13 @@ try:
     from selectAudioDevice import selectAudioDevice
 except ImportError as e:
     module_name = str(e).split('No module named ', 1)[1].strip().strip('"\'')
-    print(f'Missing module {module_name}. Please download at')
-    print(f'https://github.com/Daniel-Chin/Python_Lib/blob/master/{module_name}.py')
-    input('Press Enter to quit...')
+    if module_name in (
+        'interactive', 'yin', 'streamProfiler', 
+        'hybridSynth', 'selectAudioDevice', 
+    ):
+        print(f'Missing module {module_name}. Please download at')
+        print(f'https://github.com/Daniel-Chin/Python_Lib')
+        input('Press Enter to quit...')
     raise e
 
 print('Preparing...')
